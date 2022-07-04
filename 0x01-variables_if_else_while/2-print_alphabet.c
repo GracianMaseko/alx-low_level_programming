7935e91faec9fa88bc0c2540ddd74d77dd6d6d3e
GracianMaseko <gracianmaseko7@gmail.com>
2022-07-04 14:19:46 -0700

    2. alphabet game.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *   
+ * 
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -10,8 +10,8 @@ int main(void)
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-			putchar(c);
-			putchar('\n');
+		putchar(c);
+		putchar('\n');
 
 	return (0);
 }
